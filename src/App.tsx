6445d6bdcffec6c9b9ef7de623f460c0b1f53d60
Filip Franicevic <fliksauskas22@gmail.com>
2022-06-18 18:53:44 +0200

    adding hooks and handle function

--- src/App.tsx
+++ src/App.tsx
@@ -27,6 +27,29 @@ const transformData = (data: any) => {
   });
 };
 
+const [coins, setCoins] = useState<CoinType[]>([]);
+const [search, setSearch] = useState<string>('');
+const [isFetched, setIsFetched] = useState<boolean>(false);
+
+useEffect(() => {
+  const fetchCoins = async () => {
+    const result = await axios(
+      `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1`
+    );
+    setCoins(transformData(result.data));
+    setIsFetched(true);
+  };
+  fetchCoins();
+}, []);
+
+const handleChange = (e: any) => {
+  setSearch(e.target.value);
+};
+
+const filteredCoins = coins.filter((coin) =>
+  coin.name.toLowerCase().includes(search.toLowerCase())
+);
+
 const App: React.FC = () => {
   return <div className="App">Hello World!</div>;
 };
