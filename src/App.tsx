b2087225d5e4684081d101e33f9ef22f32995e05
Filip Franicevic <fliksauskas22@gmail.com>
2022-06-18 18:52:47 +0200

    setting the inteface

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const App = () => {
+interface CoinType {
+  currentPrice: number;
+  id: string;
+  image: string;
+  marketCap: number;
+  name: string;
+  priceChangePercentage24H: number;
+  symbol: string;
+  totalVolume: number;
+}
+
+const transformData = (data: any) => {
+  return data.map((coin: any): CoinType => {
+    return {
+      currentPrice: coin.current_price,
+      id: coin.id,
+      image: coin.image,
+      marketCap: coin.market_cap,
+      name: coin.name,
+      priceChangePercentage24H: coin.price_change_percentage_24h,
+      symbol: coin.symbol,
+      totalVolume: coin.total_volume,
+    };
+  });
+};
+
+const App: React.FC = () => {
   return <div className="App">Hello World!</div>;
 };
 
